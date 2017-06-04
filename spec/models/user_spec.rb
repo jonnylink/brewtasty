35866d7e7b690b0563c2b3dd83438ddefb958bc7
Jon Link <jonlink@gmail.com>
2017-06-04 19:58:46 -0400

    added can_brew? method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,46 @@
 require 'spec_helper'
 
 describe User do
-  it "has a valid factory" do
-    expect(FactoryGirl.create(:user)).to be_valid
-  end
-
   it {expect validate_presence_of(:username)}
   it {expect validate_presence_of(:email)}
   it {expect validate_uniqueness_of(:email)}
   it {expect validate_uniqueness_of(:username)}
+
+
+  it "has a valid factory" do
+    expect(FactoryGirl.create(:user)).to be_valid
+  end
+
+  it "returns true if a user has the inventory to brew a beer" do
+    user = FactoryGirl.create(:user)
+    unit = FactoryGirl.create(:unit)
+    category = FactoryGirl.create(:category)
+    ingredient = FactoryGirl.create(:ingredient, category: category)
+    inventory = FactoryGirl.create(:inventory,
+      user: user,
+      ingredient: ingredient,
+      unit: unit
+    )
+    recipe = FactoryGirl.create(:recipe, user: user)
+    recipe_ingredient = FactoryGirl.create(:recipe_ingredient,
+      recipe: recipe,
+      ingredient: ingredient,
+      unit: unit
+    )
+    expect(user.can_brew?(recipe)).to eq true
+  end
+
+  it "returns false if a user doesn't have the inventory to brew a beer" do
+    user = FactoryGirl.create(:user)
+    unit = FactoryGirl.create(:unit)
+    category = FactoryGirl.create(:category)
+    ingredient = FactoryGirl.create(:ingredient, category: category)
+    recipe = FactoryGirl.create(:recipe, user: user)
+    recipe_ingredient = FactoryGirl.create(:recipe_ingredient,
+      recipe: recipe,
+      ingredient: ingredient,
+      unit: unit
+    )
+    expect(user.can_brew?(recipe)).to eq false
+  end
 end
